f3ad7b46da38f35187a2a92550e6184e7b891191
artemkriachko <kriachkoartem@gmail.com>
2022-06-19 19:44:04 +0300

    01

--- scripts/xyz.js
+++ scripts/xyz.js
@@ -1,6 +1,12 @@
 
 if (typeof DeviceMotionEvent.requestPermission === 'function'){
     alert('function')
+    DeviceMotionEvent.requestPermission()
+        .then(permition =>{
+            alert(permition)
+        }).catch(permition =>{
+        console.log(permition)
+    })
 } else {alert('not function')}
 
 
