a5a7370b947f2c0042d321fed22b76e390af9928
artemkriachko <kriachkoartem@gmail.com>
2022-06-19 17:48:56 +0300

    01

---
+++ scripts/xyz.js
@@ -0,0 +1,11 @@
+window.addEventListener('deviceorientation',(event)=>showOrient(event))
+console.log(window.DeviceOrientationEvent)
+function showOrient(event){
+    console.log(event)
+    const orientSpan = {
+        x: document.querySelector('.orientation .x'),
+        y: document.querySelector('.orientation .y'),
+        z: document.querySelector('.orientation .z'),
+    };
+
+}
