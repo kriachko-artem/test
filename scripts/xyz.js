38e55a7dc2881178e33a3eadaa165a5441c32bc8
artemkriachko <kriachkoartem@gmail.com>
2022-06-19 21:53:16 +0300

    01

--- scripts/xyz.js
+++ scripts/xyz.js
@@ -1,18 +1,26 @@
 
 document.querySelector('.getPerm').addEventListener('click',()=>{
     if (typeof DeviceMotionEvent.requestPermission === 'function'){
-        document.querySelector('#x').innerHTML = 'is Function'
+        document.querySelector('.message').innerHTML = 'is Function'
         DeviceMotionEvent.requestPermission()
             .then(permission =>{
                 alert(permission)
+                window.addEventListener('devicemotion',getOrientation)
             }).catch(permission =>{
             console.log(permission)
         })
     } else {
-        document.querySelector('#x').innerHTML = 'is NOT Function'
+        document.querySelector('.message').innerHTML = 'is NOT Function'
         alert('not function')
     }
 })
+function getOrientation(event){
+    document.querySelector('#x').innerHTML = `event.alfa ${event.alpha}`
+    document.querySelector('#y').innerHTML = `event.beta ${event.beta}`
+    document.querySelector('#z').innerHTML = `event.alfa ${event.gamma}`
+
+}
+
 
 
 
