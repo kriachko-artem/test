3f8ac871acac69a8bb170d3fb2a7ca8e969ab83d
artemkriachko <kriachkoartem@gmail.com>
2022-06-19 19:23:23 +0300

    01

--- scripts/xyz.js
+++ scripts/xyz.js
@@ -1,25 +1,14 @@
 
 
-DeviceMotionEvent.requestPermission()
-    .then(response => {
-        if (response == 'granted') {
-            window.addEventListener('devicemotion', (e) => {
-                alert(e.acceleration)
-            })
-        }
-    })
-    .catch (console.error)
-
-
-
-console.log(window.DeviceOrientationEvent)
-function showOrient(event){
-    console.log(event)
-    const orientSpan = {
-        x: document.querySelector('.orientation .x'),
-        y: document.querySelector('.orientation .y'),
-        z: document.querySelector('.orientation .z'),
-    };
-    orientSpan.x.innerHTML = `event.alpha ${event.alpha}`
-
+if (typeof DeviceMotionEvent.requestPermission === "function"){
+    DeviceMotionEvent.requestPermission()
+        .then(permitionState =>{
+            if (permitionState === 'granted'){
+                alert('good')
+            } else {
+                alert('bad')
+            }
+        })
 }
+
+
