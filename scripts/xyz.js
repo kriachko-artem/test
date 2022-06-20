bd265c8296c4ab1a170dc69da474d47847bfb64b
artemkriachko <kriachkoartem@gmail.com>
2022-06-20 14:34:42 +0300

    01

--- scripts/xyz.js
+++ scripts/xyz.js
@@ -22,17 +22,18 @@ function getOrientation(){
     window.addEventListener('deviceorientation',(event)=>{
         if (initialOffset.x === null){
             console.log(event)
-            console.log(event.absolute)
-
             initialOffset.x = event.gamma;
             initialOffset.y = event.beta;
         }
-        let transformX = initialOffset.x - event.gamma,
-            transformY = initialOffset.y - event.beta;
-        gsap.to('#box',{
-            transform: `rotateY(${transformX}deg) rotateX(${transformY}deg)`,
-            duration: 0.2,
-        });
+        if ((event.beta > 0 && event.beta < 80)&&
+            (event.gamma > -20 && event.gamma < 20)){
+            let transformX = initialOffset.x - event.gamma,
+                transformY = initialOffset.y - event.beta;
+            gsap.to('#box',{
+                transform: `rotateY(${transformX}deg) rotateX(${transformY}deg)`,
+                duration: 0.2,
+            });
+        }
         document.querySelector('#x').innerHTML = `event.alfa ${parseInt(event.alpha)}`
         document.querySelector('#y').innerHTML = `event.beta ${parseInt(event.beta)}`
         document.querySelector('#z').innerHTML = `event.gamma ${parseInt(event.gamma)}`
