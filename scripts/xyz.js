fb0c642ef6a8944209c03b281cad5590712dd485
artemkriachko <kriachkoartem@gmail.com>
2022-06-20 14:28:16 +0300

    01

--- scripts/xyz.js
+++ scripts/xyz.js
@@ -1,8 +1,8 @@
 
 document.querySelector('.getPerm').addEventListener('click',()=>{
-    if (typeof DeviceMotionEvent.requestPermission === 'function'){
+    if (typeof DeviceOrientationEvent.requestPermission === 'function'){
         document.querySelector('.message').innerHTML = 'is Function'
-        DeviceMotionEvent.requestPermission()
+        DeviceOrientationEvent.requestPermission()
             .then(() =>{
                 getOrientation()
             }).catch(permission =>{
@@ -19,20 +19,21 @@ function getOrientation(){
         x: null,
         y: null,
     };
-    window.addEventListener('devicemotion',(event)=>{
+    window.addEventListener('deviceorientation',(event)=>{
         if (initialOffset.x === null){
-
             console.log(event)
-            initialOffset.x = 0;
-            initialOffset.y = 0;
+            console.log(event.absolute)
+
+            initialOffset.x = event.gamma;
+            initialOffset.y = event.beta;
         }
-        initialOffset.x += event.rotationRate.gamma;
-        initialOffset.y += event.rotationRate.beta;
+        let transformX = initialOffset.x - event.gamma,
+            transformY = initialOffset.y - event.beta;
         gsap.to('#box',{
-            transform: `rotateY(${initialOffset.x}deg) rotateX(${initialOffset.y}deg)`,
+            transform: `rotateY(${transformX}deg) rotateX(${transformY}deg)`,
             duration: 0.2,
         });
-        document.querySelector('#x').innerHTML = `event.alfa ${parseInt(event.rotationRate.alpha)}`
+        document.querySelector('#x').innerHTML = `event.alfa ${parseInt(event.alpha)}`
         document.querySelector('#y').innerHTML = `event.beta ${parseInt(transformX)}`
         document.querySelector('#z').innerHTML = `event.gamma ${parseInt(transformY)}`
     })
